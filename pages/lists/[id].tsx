638b40b6995db776591634ce90fe0b181a1a2285
MartinPettersen <searchingtheskyfor@gmail.com>
2022-06-24 01:29:27 +0200

    f

--- pages/lists/[id].tsx
+++ pages/lists/[id].tsx
@@ -19,7 +19,7 @@ interface ITodoList {
   url: string;
   tasks: Array<ITask>;
 }
-/*
+
 export const getStaticPaths = async (context: string) => {
   console.log("the context " + context);
   console.log(context);
@@ -36,8 +36,8 @@ export const getStaticPaths = async (context: string) => {
     paths,
     fallback: false,
   };
-};*/
-/*
+};
+
 export const getStaticProps = async (context: any) => {
   const id = context.params.id;
   const res = await fetch(`https://sheltered-inlet-32387.herokuapp.com/api/list/${id}`);
@@ -46,8 +46,9 @@ export const getStaticProps = async (context: any) => {
     props: { todoList: data[0] },
   };
 };
-*/
 
+
+/*
 export const getServerSideProps = async (context: any) => {
   
   const id = context.params.id;
@@ -62,6 +63,7 @@ export const getServerSideProps = async (context: any) => {
     
   };
 };
+*/
 
 type TodoListProp = {
   todoList: ITodoList;
@@ -87,7 +89,9 @@ const TodoList: NextPage<TodoListProp> = ({ todoList }) => {
   const uppdateListOfTasks = async () => {
     const tempList = await getUppdatedList();
     console.log(tempList);
-    setListOfTasks([...tempList]);
+    setListOfTasks(tempList);
+    //setListOfTasks([...tempList]);
+
     console.log("uppdating list of tasks");
   };
   
@@ -122,7 +126,7 @@ const TodoList: NextPage<TodoListProp> = ({ todoList }) => {
       */
   };
   
-  const uppdateTask = (task: ITask) => {
+  const uppdateTask = async (task: ITask) => {
     console.log("uppddating task:  " + task.taskId);
     console.log("current task status:  " + task.status);
     console.log("uppddating task:  " + task.title);
@@ -133,7 +137,8 @@ const TodoList: NextPage<TodoListProp> = ({ todoList }) => {
     } else {
       tempStatus = "todo";
     }
-    fetch(`https://sheltered-inlet-32387.herokuapp.com/api/list/add/${todoList.url}`, {
+    
+    await fetch(`https://sheltered-inlet-32387.herokuapp.com/api/list/uppdate/${todoList.url}`, {
       method: 'POST',
       headers: {
         Accept: 'application.json',
@@ -147,6 +152,9 @@ const TodoList: NextPage<TodoListProp> = ({ todoList }) => {
       })
       .then(() => console.log("uppdating task status"))
       .then(() => uppdateListOfTasks())
+      .catch((err) => {
+        console.error(err);
+      });
     /*
     Axios.post(`https://sheltered-inlet-32387.herokuapp.com/api/list/uppdate/${todoList.url}`, {
       status: tempStatus,
@@ -160,87 +168,18 @@ const TodoList: NextPage<TodoListProp> = ({ todoList }) => {
       */
   };
 
-  /*
-  <div>
-        <p className="red important">
-          Keep this url to access this todoList. You can send it to others if
-          you want share your todolist
-        </p>
-      </div>
-      <p className="blue important">URL: /lists/{todoList.url}</p>
-      <h1>Todo List: </h1>
-      <h2>Title: {todoList.title}</h2>
-      <h3>Desc: {todoList.description}</h3>
-      <div>
-        <h3>Create a new Task:</h3>
-        <div className="inputContainer">
-          <div className="wrapper">
-            <p className="text orange">Title:</p>
-            <input
-              className="taskTitle"
-              value={taskTitle}
-              onChange={(e) => setTaskTitle(e.target.value)}
-            />
-          </div>
-          <div className="wrapper">
-            <p className="text orange">Description:</p>
-            <input
-              className="taskDescription"
-              value={taskDescription}
-              onChange={(e) => setTaskDescription(e.target.value)}
-            />
-          </div>
-          <p onClick={() => createTask()} className="orange-border">
-            Create Task
-          </p>
-        </div>
-      </div>
-      <div>
-        <div className="wrapper">
-          <h3 className="colonHeader blue-background todoContainer">Todo</h3>
-          <h3 className="colonHeader red-background finishedContainer">Done</h3>
-        </div>
-        <div className="wrapper">
-          <div className="todoContainer taskContainer">
-            <div>
-              {lisOfTasks
-                .filter((task) => task.status === "todo")
-                .map((task) => (
-                  <Task
-                    key={task.taskId}
-                    uppdateTask={uppdateTask}
-                    todoTask={task}
-                  />
-                ))}
-            </div>
-          </div>
-          <div className="finishedContainer taskContainer">
-            {lisOfTasks
-              .filter((task) => task.status === "done")
-              .map((task) => (
-                <Task
-                  key={task.taskId}
-                  uppdateTask={uppdateTask}
-                  todoTask={task}
-                />
-              ))}
-          </div>
-        </div>
-      </div>
-      /*/
 
-  /*
-  */
   useEffect(() => {
     uppdateListOfTasks();
     console.log("First render");
   }, []);
+
   useEffect(() => {
     console.log("rerender");
   }, [setListOfTasks]);
+
   return (
     <div>
-      <p>test</p>
       <div>
         <p className="red important">
           Keep this url to access this todoList. You can send it to others if
